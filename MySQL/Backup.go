b1c28c856940a1284f1bcb00c024d61bd541128d
srikary12 <121927567+srikary12@users.noreply.github.com>
2024-09-28 15:13:57 +0530

    fixed mysql backup

--- MySQL/Backup.go
+++ MySQL/Backup.go
@@ -11,19 +11,19 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
-func Backup(args globals.DbArgs) () {
+func Backup(args globals.DbArgs) {
 	cfg := mysql.Config{
 		User:   args.Username,
 		Passwd: args.Password,
 		Net:    "tcp",
-		Addr:   args.Host.IP.String() + args.Port,
+		Addr:   args.Host.IP.String() + ":" + args.Port,
 		DBName: args.DbName,
 	}
 	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer db.Close()
 
 	if err := db.Ping(); err != nil {
@@ -31,20 +31,24 @@ func Backup(args globals.DbArgs) () {
 	}
 	var cmd *exec.Cmd
 	if len(args.TableNames) == 0 {
-		cmd = exec.Command("mysqldump", "-host", args.Host.IP.String(), "-u", args.Username, "--port", string(args.Port), "-p", args.Password, "--all-databases")
+		cmd = exec.Command("mysqldump",
+			"--host", args.Host.IP.String(),
+			"--user", args.Username,
+			"--password="+args.Password,
+			"-P", string(args.Port),
+			args.DbName)
 	} else {
 		cmd = exec.Command("mysqldump", "-host", args.Host.IP.String(), "-u", args.Username, "--port", string(args.Port), "-p", args.Password)
 		cmd.Args = append(cmd.Args, args.TableNames...)
 	}
-	
 	//todo: add option to select the folder and compression
 	outfile, err := os.Create("backup.sql")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outfile.Close()
-	cmd.Stdout = outfile
 
+	cmd.Stdout = outfile
 	err = cmd.Start(); if err != nil {
 		log.Fatal(err)
 	}
@@ -52,4 +56,5 @@ func Backup(args globals.DbArgs) () {
 	cmd.Wait()
 	fmt.Println("Backup Completed")
 }
+
 //Restore
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	"dbBackupUtility/MySQL"
 	"dbBackupUtility/globals"
 )
 
 func main() {
-	fmt.Println("Hi Hello")
 	var args globals.DbArgs
-	fmt.Println(args)
 	mysql.Backup(args)
 }
